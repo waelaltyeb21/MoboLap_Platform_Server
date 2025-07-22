624fef81c055a67d42ed01db0223c69901458cc4
waelaltyeb21 <waelaltyeb2124@gmail.com>
2025-07-22 14:38:25 +0200

    Delete client directory

--- client/src/app/compare/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Compare = () => {
-  return <section>Compare</section>;
-};
-
-export default Compare;
--- client/src/app/offers/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Offers = () => {
-  return (
-    <section>Offers</section>
-  )
-}
-
-export default Offers
