302d795d52a466c43b9aa0e30c7777e705c8a22b
waelaltyeb21 <waelaltyeb2124@gmail.com>
2025-07-22 11:02:37 +0200

    Delete client - with shadcn directory

--- client - with shadcn/src/app/products/[id]/page.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const ProductDetails = async ({ params }) => {
-  const { id } = await params;
-  return <section>Product Details - {id}</section>;
-};
-
-export default ProductDetails;
