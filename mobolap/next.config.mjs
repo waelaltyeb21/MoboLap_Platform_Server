f4bc801c719cbb875304f6da598753a1a85cae96
waelaltyeb21 <waelaltyeb2124@gmail.com>
2025-07-27 14:52:30 +0200

    Delete mobolap directory

--- mobolap/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  devIndicators: false,
-  images: {
-    domains: ["google.com"],
-  },
-};
-
-export default nextConfig;
--- mobolap/src/app/products/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Products() {
-  return (
-    <section className="Products">
-      <h1 className="text-xl text-center font-medium mt-16">صفحة منتجات المتجر</h1>
-    </section>
-  );
-}
