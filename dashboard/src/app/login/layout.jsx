09728ecd97e661d744dbda9cbb719fba05a7a343
waelaltyeb21 <waelaltyeb2124@gmail.com>
2025-07-29 08:56:52 +0200

    Delete dashboard directory

--- dashboard/src/app/login/layout.jsx
+++
@@ -1,6 +0,0 @@
-import LoginLayout from "@/Layouts/LoginLayout";
-import React from "react";
-
-export default function DashboardLogin({ children }) {
-  return <LoginLayout>{children}</LoginLayout>;
-}
