946fc4a7588d905045fa9f4ee30759c0a173ab73
waelaltyeb21 <waelaltyeb2124@gmail.com>
2025-08-05 13:05:31 +0200

    Delete dashboard directory

--- dashboard/src/components/Products/ProductDetails/ProductExtension.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ProductExtension = ({ product, register, control, errors }) => {
-  return <div className="Product_Extension">Product Extension</div>;
-};
-
-export default ProductExtension;
