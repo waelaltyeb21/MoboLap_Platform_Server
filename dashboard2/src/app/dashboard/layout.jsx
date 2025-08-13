4e533646c1b2ca81a48761e6563e091cc443ef61
waelaltyeb21 <waelaltyeb2124@gmail.com>
2025-08-13 16:01:21 +0200

    Delete dashboard2 directory

--- dashboard2/src/app/dashboard/layout.jsx
+++
@@ -1,6 +0,0 @@
-import AdminLayout from "@/Layouts/AdminLayout";
-import React from "react";
-
-export default function DashboardLayout({ children }) {
-  return <AdminLayout>{children}</AdminLayout>;
-}
--- dashboard2/src/app/dashboard/page.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function DashboardHomePage() {
-  return <section>Home Page</section>;
-}
